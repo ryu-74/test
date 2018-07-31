4947bad13ae31cabc4b30e84b62fcc26ace35934
ryu-74 <nisifumi74@gma.com>
2018-07-31 12:58:34 +0900

    挨拶を追加その3

--- Exec.java
+++ Exec.java
@@ -2,6 +2,8 @@ package exsample01;
 public class Exec{
 	public static void main(String[]args){
 		System.out.println("挨拶:おはよう");
+		System.out.println("挨拶:こんにちは");
+		System.out.println("挨拶:こんばんわ");
 	}
 }
 		
